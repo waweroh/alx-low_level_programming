e52613a803511aacd6d287571afc53768401c1c4
waweroh <scadmsez@gmail.com>
2021-08-26 11:47:09 -0700

    C program that prints using printf and returns a new line.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main -Entry point 
+ * main - Entry point 
  *
  * Return: Always 0 (Success)
  */
@@ -9,4 +9,5 @@ int main(void)
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
+
 
