d19edeedc53f8d1283216f0dc4131679ea558d85
waweroh <scadmsez@gmail.com>
2021-08-31 10:24:11 -0700

    print holberton return 0 if successful

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *main - entry point
+ *write a program that prints holberton
+ *return 0 on success
+ */
+int main(void)
+{
+  char name[] = "_putchar";
+  int i;
+
+  i = 0;
+  while (name[i] != '\0')
+    {
+      
+      _putchar(name[i]);
+      i++;
+      
+    }
+  _putchar('\n');
+  return (0);
+}    
+  
