fd3501a75e745a29bf4c53e732ff6c6d5ac6f094
waweroh <scadmsez@gmail.com>
2022-05-12 05:12:52 +0300

    header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,14 @@
 #include <stdlib.h>
 #include <math.h>
 
+/* STRUCTS */
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ */
 typedef struct listint_s
 {
 	int n;
@@ -12,6 +20,17 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
 
 typedef struct skiplist_s
 {
